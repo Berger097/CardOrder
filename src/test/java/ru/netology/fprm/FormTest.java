090fc175ef12c962af740e8d893ebecfa478b659
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2022-03-24 17:26:10 +0600

    commit 1

--- src/test/java/ru/netology/fprm/FormTest.java
+++ src/test/java/ru/netology/fprm/FormTest.java
@@ -38,7 +38,7 @@ public class FormTest {
         driver.findElement(By.cssSelector("[data-test-id='name'] input")).sendKeys("Елена");
         driver.findElement(By.cssSelector("[data-test-id='phone'] input")).sendKeys("+79994702121");
         driver.findElement(By.cssSelector("[data-test-id='agreement']")).click();
-        driver.findElement(By.cssSelector("[data-test-id='submit']")).click();
+        driver.findElement(By.cssSelector("[type='button']")).click();
         String text = driver.findElement(By.cssSelector("[data-test-id='callback-success']")).getText();
         String expected = "Ваша заявка успешно отправлена!";
         Assertions.assertEquals(expected, text.trim());
