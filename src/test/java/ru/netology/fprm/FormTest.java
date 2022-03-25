97054cf7c40612e1f88e5a4343a3e538d9a46ec4
Anatoly Moskalev <moskalev1997tolya@gmail.com>
2022-03-25 13:42:12 +0600

    DD

--- src/test/java/ru/netology/fprm/FormTest.java
+++ src/test/java/ru/netology/fprm/FormTest.java
@@ -26,7 +26,7 @@ public class FormTest {
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
 
     }
 
